804c6bb8d6c63c3a4d4c6d3377601bd44fb0eeea
William W Wold <wilcoze@gmail.com>
2017-08-08 04:58:44 -0700

    added function to clean up names

--- simple_run_menu.py
+++ simple_run_menu.py
@@ -21,6 +21,26 @@ def get_files_in_dir(directory):
 		directory += '/'
 	return [directory + i for i in os.listdir(directory)]
 
+def command_to_name(command):
+	filename_with_ext = os.path.basename(command)
+	filename = filename_with_ext.rsplit('.', 1)[0]
+	name = filename.replace('_', ' ')
+	capitalized = ' '.join([i[0].upper() + i[1:] for i in name.split()])
+	return capitalized
+	
+class Option:
+	
+	options = {}
+	
+	@staticmethod
+	def add(command):
+		options['a'] = Option(command, command, 'a')
+	
+	def __init__(self, name, command, trigger):
+		self.name = name
+		self.command = command
+		self.trigger = trigger
+	
 if __name__ == "__main__":
-	print([i for i in get_files_in_dir('') if is_file_executable(i)])
+	print([command_to_name(i) for i in get_files_in_dir('') if is_file_executable(i)])
 	
